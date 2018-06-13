4df38a25ff1fe3634551b6b556d7f873e883c6af
Konstantin BIFERT <hello@kissu.io>
2018-06-13 23:58:07 +0200

    Ugly but kinda working

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,49 @@
   #app
     img(src='./assets/logo.png')
     p(v-for="flavor in flavors" :key="flavor.id") {{ flavor.children }}
-    button(@click="") Click me
-    button(@click="level-= 1") Decrement
-    button(@click="level+= 1") Increment
-    br
-    br
-    div {{ level }}
+    label level
+    input(v-model.number="level")
+    label name
+    input(v-model="name")
     hr
-    div {{ currentRing }}
+    div currentRing: {{ currentRing }}
+    hr
+    hr
+    div nested var: {{ nice }}
 </template>
 
 <script>
 import flavorData from './data.json'
-
+/* eslint-disable */
 export default {
   name: 'App',
   data() {
     return {
       flavors: flavorData,
-      level: 2,
-      name: 'miel',
+      level: 0,
+      name: '',
+      nice: '',
     }
   },
   computed: {
     currentRing() {
-      let recursive_find = child => {
-        if (child.name !== this.name || child.level !== this.level) {
-          return child.children.find(recursive_find)
+      let recursive_find = flavorElement => {
+        if (flavorElement.level === this.level) {
+          if (flavorElement.name === this.name) {
+            console.log('good name')
+            return (this.nice = flavorElement)
+          } else {
+            console.log('not the good name')
+            return
+          }
         }
         // else if (child.level !== this.level && child.name !== this.name) {
         //   return child.children.find(recursive_find)
         // }
-        return child
+        console.log('ext return')
+        return flavorElement.children.find(recursive_find)
       }
-      return this.flavors.find(recursive_find)
+      return this.level === 0 ? this.flavors[0].children : this.flavors.find(recursive_find)
     },
   },
 }
