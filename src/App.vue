c4a65d38f983c2ec0ebe2802caa7ae2d1bc85b09
Konstantin BIFERT <hello@kissu.io>
2018-06-22 00:24:15 +0200

    Connais-tu le sens du mot FOLIE ? :) Si jamais mon PC crash encore... xD

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template lang='pug'>
   #app
-    .wheel()
-      svg(:width="400" :height="400" viewBox="0 0 400 400")
-        //-
-          cannot use svgSize here ??!
-        circle(:cx="svgSize" :cy="svgSize" :r="svgSize" stroke="white" fill="teal" stroke-width="2")
-        //- rect(:x="400" :y="svgSize" width="10" height="3" stroke="white" fill="red" stroke-width="2")
-        path(
-          v-for="(flavorQuarter, index) in testArray"
-          :class="'wheelQuarters' + index"
-          :index="index + 1"
-          :fill="colors[index]"
-          stroke="white"
-          stroke-width="2"
-          transform="rotate(0 0 0)"
-        )
-        text(
-          v-for="(flavorQuarter, index) in testArray"
-          :class="'wheelQuartersText' + index"
-          font-size="18px"
-          fill="orange"
-          :x="svgSize + 10"
-          :y="svgSize + 5"
-        ) number # {{ flavorQuarter }}
+    svg.wheel(:width="400" :height="400" viewBox="0 0 400 400")
+      //-
+        cannot use svgSize here ??!
+      circle(:cx="svgSize" :cy="svgSize" :r="svgSize" stroke="white" fill="teal" stroke-width="2")
+      //- rect(:x="400" :y="svgSize" width="10" height="3" stroke="white" fill="red" stroke-width="2")
+      path(
+        v-for="(flavorQuarter, index) in flavors.children"
+        @click.stop="chooseFlavor(index)"
+        :class="'wheelQuarters' + index"
+        :index="index"
+        :fill="colors[index]"
+        transform="rotate(0 0 0)"
+      )
+
+      // this part is REALLY garbage and should be split in 2 components and with the use of https://developer.mozilla.org/en-US/docs/Web/SVG/Element/textPath
+
+      text(
+        v-for="(flavorQuarter, index) in flavors.children"
+        @click.stop="chooseFlavor(index)" /* this one can be omitted imo */
+        :class="'wheelQuartersText' + index"
+        font-size="18px"
+        fill="orange"
+        :x="svgSize + 50"
+        :y="svgSize + 5"
+        text-align="center"
+        transform="rotate(-45 0 0) translate(15 0)"
+      ) {{ flavorQuarter.name }}
+
+      circle.backPrevious(
+        @click.stop='backPreviousFlavor'
+        :cx="svgSize"
+        :cy="svgSize"
+        :r="20"
+        stroke="white"
+        fill="red"
+        stroke-width="2"
+      )
+
+    label level
+    input(v-model.number="level")
+    p {{ level }}
+    label name
+    input(v-model="name")
+    p {{ name }}
+    button(@click="recursive_find") Look for the items
+    hr
+    div All the available flavors:
+    div(v-for="result in results.children") {{ result.name }}
+    hr
 
 </template>
 
@@ -38,17 +64,37 @@ export default {
       flavors: flavorData,
       level: 0,
       name: '',
-      colors: ['#fff', '#EF476F', '#456990', '#06D6A0', 'yellow', '#073B4C', '#028090', '#F45B69'],
-      // testArray: ['a', 'b', 'c', 'd', 'a1', 'b1', 'c1', 'd1'],
-      testArray: ['0', '1', '2', '3', '4', '5'],
+      results: '',
+      colors: [
+        '#fff',
+        '#EF476F',
+        '#456990',
+        '#06D6A0',
+        'yellow',
+        '#073B4C',
+        '#028090',
+        '#F45B69',
+        'yellow',
+        '#073B4C',
+        '#028090',
+        '#F45B69',
+        'yellow',
+        '#073B4C',
+        '#028090',
+        '#F45B69',
+      ],
+      // flavors: {
+      //   children: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
+      // },
       angle: 0,
       svgSize: 200,
-      lol: 0,
+      // I missed some important stuff, so...I gonna make this ugly fix.......
+      uglyOffset: [0, 0, -30, -40, -50, 0, -65, -68, -70, -72, -74, -75, -76, -76, -77],
     }
   },
   computed: {
     radialVelocity() {
-      return 360 / this.testArray.length
+      return 360 / this.flavors.children.length
     },
   },
   // https://codepen.io/olufotebig/pen/YVjJpE?editors=1010
@@ -61,32 +107,24 @@ export default {
     region.bind(wheel, spinOfTheHell, function(e) {
       currentAngle += e.detail.distanceFromLast
       this.lol = e.detail.distanceFromLast
-      console.log(this.lol)
-
       wheel.style.transform = `rotate(${currentAngle}deg)`
 
-      // var textLabel1 = document.querySelector('.potato1')
-      // textLabel1.setAttribute('transform', `rotate(${360}, svgSize, 200)`, `translate(200 200)`)
-      // var textLabel2 = document.querySelector('.potato2')
-      // textLabel2.setAttribute('transform', `rotate(${180}, 200, 200)`, `translate(200 200)`)
-      // var wheelQuarters = document.querySelectorAll("[class*='wheelQuarters']")
-      // wheelQuarters.forEach(oneWheel =>
-      //   oneWheel.setAttribute('transform', `rotate(${0 + 120 * oneWheel.getAttribute('index')})`),
-      // )
-      // console.log(e.detail.angle)
+      console.log(currentAngle)
     })
-    // console.log(this.radialVelocity)
+    var middleBackButton = document.querySelector('.backPrevious')
+    region.unbind(middleBackButton)
 
-    this.testArray.forEach((flavor, index) =>
+    Object.values(this.flavors.children).forEach((flavor, index) =>
       document
         .querySelector(`.wheelQuartersText${index}`)
         .setAttribute(
           'transform',
-          `rotate(${this.radialVelocity * index - this.lol * 5} 200 200) translate(15 0)`,
+          `rotate(${this.radialVelocity * index +
+            this.uglyOffset[this.flavors.children.length - 1]} 200 200) translate(15 0)`,
         ),
     )
 
-    this.testArray.forEach((flavor, index) =>
+    Object.values(this.flavors.children).forEach((flavor, index) =>
       document
         .querySelector(`.wheelQuarters${index}`)
         .setAttribute(
@@ -101,10 +139,25 @@ export default {
         ),
     )
   },
+  methods: {
+    chooseFlavor(index) {
+      // this.level += 1
+    },
+    backPreviousFlavor() {},
+    recursive_find() {
+      let rfind = flavorElement => {
+        if (flavorElement.level === this.level && flavorElement.name === this.name) {
+          return (this.results = flavorElement)
+        }
+        return flavorElement.children.find(rfind)
+      }
+      return this.level === 0 ? (this.results = this.flavors) : this.flavors.children.find(rfind)
+    },
+  },
 }
 
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  var angleInRadians = (angleInDegrees - 50) * Math.PI / 180.0
+  var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0
 
   return {
     x: centerX + radius * Math.cos(angleInRadians),
