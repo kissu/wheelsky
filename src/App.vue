b3165d408b610cd97558f5226104e0eb2b777a79
Konstantin BIFERT <hello@kissu.io>
2018-06-21 02:20:31 +0200

    meh...

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template lang='pug'>
   #app
-    .wheel(@dblclick="test")
+    .wheel()
       svg(:width="400" :height="400" viewBox="0 0 400 400")
-        circle(:cx="200" :cy="200" r="200" stroke="white" fill="teal" stroke-width="2")
-        //- rect(:x="400" :y="200" width="10" height="3" stroke="white" fill="red" stroke-width="2")
+        //-
+          cannot use svgSize here ??!
+        circle(:cx="svgSize" :cy="svgSize" :r="svgSize" stroke="white" fill="teal" stroke-width="2")
+        //- rect(:x="400" :y="svgSize" width="10" height="3" stroke="white" fill="red" stroke-width="2")
         path(
           v-for="(flavorQuarter, index) in testArray"
           :class="'wheelQuarters' + index"
+          :index="index + 1"
           :fill="colors[index]"
           stroke="white"
           stroke-width="2"
+          transform="rotate(0 0 0)"
         )
         text(
-          v-for="(flavorQuarter, index) in testArray[0]"
-          :class=""
-          transform="rotate(-45 0 0) translate(0, 150)"
+          v-for="(flavorQuarter, index) in testArray"
+          :class="'wheelQuartersText' + index"
           font-size="18px"
           fill="orange"
-        ) wonderful {{ flavorQuarter }}
+          :x="svgSize + 10"
+          :y="svgSize + 5"
+        ) number # {{ flavorQuarter }}
 
 </template>
 
@@ -33,8 +38,12 @@ export default {
       flavors: flavorData,
       level: 0,
       name: '',
-      colors: ['#fff', '#EF476F', '#456990', '#06D6A0', '#118AB2', '#073B4C', '#028090', '#F45B69'],
-      testArray: ['a', 'b', 'c', 'd', 'a1', 'b1', 'c1', 'd1'],
+      colors: ['#fff', '#EF476F', '#456990', '#06D6A0', 'yellow', '#073B4C', '#028090', '#F45B69'],
+      // testArray: ['a', 'b', 'c', 'd', 'a1', 'b1', 'c1', 'd1'],
+      testArray: ['0', '1', '2', '3', '4', '5'],
+      angle: 0,
+      svgSize: 200,
+      lol: 0,
     }
   },
   computed: {
@@ -42,6 +51,7 @@ export default {
       return 360 / this.testArray.length
     },
   },
+  // https://codepen.io/olufotebig/pen/YVjJpE?editors=1010
   mounted() {
     var wheel = document.querySelector('.wheel')
     var region = new ZingTouch.Region(wheel)
@@ -50,15 +60,31 @@ export default {
 
     region.bind(wheel, spinOfTheHell, function(e) {
       currentAngle += e.detail.distanceFromLast
+      this.lol = e.detail.distanceFromLast
+      console.log(this.lol)
 
-      wheel.style.transform = 'rotate(' + currentAngle + 'deg)'
+      wheel.style.transform = `rotate(${currentAngle}deg)`
 
-      // var textLabel = document.querySelector('.flavorLabel')
-      // textLabel.style.transform = 'rotate(' + currentAngle + 'deg)'
-
-      console.log(e.detail.angle)
+      // var textLabel1 = document.querySelector('.potato1')
+      // textLabel1.setAttribute('transform', `rotate(${360}, svgSize, 200)`, `translate(200 200)`)
+      // var textLabel2 = document.querySelector('.potato2')
+      // textLabel2.setAttribute('transform', `rotate(${180}, 200, 200)`, `translate(200 200)`)
+      // var wheelQuarters = document.querySelectorAll("[class*='wheelQuarters']")
+      // wheelQuarters.forEach(oneWheel =>
+      //   oneWheel.setAttribute('transform', `rotate(${0 + 120 * oneWheel.getAttribute('index')})`),
+      // )
+      // console.log(e.detail.angle)
     })
-    console.log(this.radialVelocity)
+    // console.log(this.radialVelocity)
+
+    this.testArray.forEach((flavor, index) =>
+      document
+        .querySelector(`.wheelQuartersText${index}`)
+        .setAttribute(
+          'transform',
+          `rotate(${this.radialVelocity * index - this.lol * 5} 200 200) translate(15 0)`,
+        ),
+    )
 
     this.testArray.forEach((flavor, index) =>
       document
@@ -75,15 +101,10 @@ export default {
         ),
     )
   },
-  methods: {
-    test(e) {
-      console.log(e)
-    },
-  },
 }
 
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0
+  var angleInRadians = (angleInDegrees - 50) * Math.PI / 180.0
 
   return {
     x: centerX + radius * Math.cos(angleInRadians),
@@ -122,6 +143,9 @@ function describeArc(x, y, radius, startAngle, endAngle) {
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
 body {
   background-color: #000;
 }
@@ -135,8 +159,5 @@ body {
 }
 .wheelsky {
   fill: yellowgreen;
-}
-.wheel-middle {
-  border: 3px solid red;
 }
 </style>
