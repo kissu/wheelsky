010dd4ec2c4b70052a046744424d3908e03ef541
Konstantin BIFERT <konstantin.bifert@gmail.com>
2019-03-06 23:01:45 +0100

    Update some components

---
+++ src/components/Wheelsky.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <svg viewBox="-500 -500 1000 1000" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M 420.71067811865476 279.28932188134524
+    A 100 100 0 0 1 420.71067811865476 420.71067811865476
+    L 350 350"
+        fill="teal"
+      ></path>
+    </svg>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+svg
+  height: 400px
+  border: 4px solid teal
+</style>
