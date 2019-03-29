f1a0ee8a6dfca17fdd14d8ca091b466e7520b16d
Konstantin BIFERT <konstantin.bifert@gmail.com>
2019-03-29 18:38:46 +0100

    Add honeypot

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,6 +1,13 @@
 <template>
-  <form name="degustation" method="post" data-netlify="true" class="w-full max-w-md center">
+  <form
+    name="degustation"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+    class="w-full max-w-md center"
+  >
     <!-- action="/merci.html" -->
+    <input type="hidden" name="form-name" value="degustation">
     <div class="flex flex-wrap -mx-3 mb-5 mt-4">
       <div class="w-full md:w-1/2 px-3 mb-5 md:mb-0">
         <label
