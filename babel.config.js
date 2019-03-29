a921a220a7116c9e155ef83b299de1bfe2d08a79
Konstantin BIFERT <konstantin.bifert@gmail.com>
2019-03-29 19:05:52 +0100

    webpack 2.0

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,3 @@
-const path = require("path");
-const PrerenderSPAPlugin = require("prerender-spa-plugin");
-
 module.exports = {
-  plugins: [
-    new PrerenderSPAPlugin({
-      // Required - The path to the webpack-outputted app to prerender.
-      staticDir: path.join(__dirname, "dist"),
-      // Required - Routes to render.
-      routes: ["/", "/merci.html"]
-    })
-  ],
   presets: ["@vue/app"]
 };
